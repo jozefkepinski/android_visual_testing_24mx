1aed8c4c77ab7d3a6ae5374918ac714794dbbf5b
jozefkepinski <jozefkepinski@hotmail.com>
2024-04-14 23:48:21 +0200

    added POM to first test and deleted unused files

---
+++ tests/24mx_pom.page.ts
@@ -0,0 +1,113 @@
+import { Android, AndroidDevice, Page, expect } from '@playwright/test';
+
+export class TwentyFourMxPage {
+    constructor(private page: Page, private device: AndroidDevice) {}
+
+    // Locators
+    readonly searchInput = this.page.locator('#search-desktop');
+    readonly searchButton = this.page.locator("(//a[@class='autocomplete-item'])[1]");
+    readonly categoryHelmets = this.page.locator("//a[@class='m-navigation-link'][normalize-space()='Kaski']")
+    readonly crossHelmets = this.page.locator("(//img[@alt='Kask Cross'])[1]")
+    readonly openTheHelmetsSizes = this.page.locator("//div[@class='m-select__display']")
+    readonly addToCart = this.page.locator(".m-button.m-button--purchase.qa-pdp-add-to-cart-btn.m-button--md")
+    readonly checkout = this.page.locator("//a[contains(text(),'Przejdź do kasy')]")
+    readonly basket = this.page.locator("//div[@class='m-header-button m-header-button--icon qa-mobile-header-cart']//fa-icon[@class='ng-fa-icon']//*[name()='svg']")
+    readonly cashbox =  this.page.locator("(//span[contains(text(),'Przejdź do kasy')])[1]")
+
+    async goto() {
+        await this.page.goto('https://www.24mx.pl/');
+        console.log(await this.page.evaluate(() => window.location.href));
+        if (await this.page.getByRole('button',{name:"OK"}).count()>0)
+        await this.page.getByRole('button',{name:"OK"}).click()
+        await this.page.getByRole('img', {name:'Kaski MX'}).click()
+        this.page.waitForLoadState('domcontentloaded')
+    }
+
+    // Methods
+    async performSearch(query: string) {
+        await this.searchInput.fill(query);
+        await this.page.waitForLoadState()
+        await this.searchButton.click();
+        await this.page.waitForLoadState()
+    }
+
+    async waitForMainPageLoadState() {
+        await this.page.waitForSelector('//*[@id="wrapper"]/div/p-home/div[1]/div[1]/p-cms-dynamic-renderer[1]/p-cms-freestyle/div/div/div/div[1]', {state:'visible'});
+        await this.page.waitForLoadState()
+    }
+
+    async compareImage(image: string, _clip?:{x: number; y: number; width: number; height: number}|undefined) {
+        if(_clip)
+            {
+                await Promise.all([
+                    expect(this.page).toHaveScreenshot(image, {clip: {x:_clip.x, y:_clip.y, height:_clip.height, width:_clip.width}})
+                ])
+            }
+        else
+        {
+            await Promise.all([expect(this.page).toHaveScreenshot(image)])
+        }
+        this.page.waitForLoadState()
+    }
+
+    async openCategoryHelmetsCross() {
+        await this.categoryHelmets.click()
+        await this.page.waitForLoadState()
+        await this.crossHelmets.click()
+
+    }
+
+    async chooseHelmetbyTitle(name:string){
+        await this.page.click(`div[class='m-product-card-img'] img[title='${name}']`);
+    }
+
+    async chooseHelmetSize(size:string){
+        /**
+         * Select Helmet size.
+         * @param size - Select size number XS=1, S:2, M:3, L:4, XL:5
+         */
+        const sizes: { [key: string]: string } = {
+            XS: "1",
+            S: "2",
+            M: "3",
+            L: "4",
+            XL: "5"
+          };
+        await this.device.shell('input swipe 10 800 10 10');
+        await this.openTheHelmetsSizes.click()
+        await this.page.click(`(//div[@class='a-product-variation'])[${sizes[size]}]`)
+    }
+
+    async addToCartAndCheckout(){
+        await this.addToCart.click()
+        await this.checkout.click()
+        await this.page.waitForLoadState()
+        await this.page.waitForSelector("//div[@class='m-checkout-box m-checkout-box--delivery']//div[@class='m-checkout-box__heading']", {state:'visible'});
+        await this.page.waitForLoadState()
+    }
+
+    async goToBasket(){
+        console.log('Go to basket')
+        await this.basket.click()
+        this.page.waitForLoadState()
+    }
+
+    async goToCashBox(){
+        console.log('Go to CashBox')
+        await this.cashbox.click()
+        this.page.waitForLoadState()
+    }
+
+    async check_address_and_contact_details(){
+        console.log('Check address and contact details')
+        await this.device.shell('input swipe 10 1000 10 10');
+        this.page.waitForLoadState()
+        await expect(this.page.getByRole('heading', {name:'1. Wybierz opcję dostawy'})).toBeVisible();
+        console.log('Checking: 1. Wybierz opcję dostawy')
+        await expect(this.page.getByRole('heading', {name:'2. Wybierz metodę płatności'})).toBeVisible();
+        console.log('Checking: 2. Wybierz metodę płatności')
+        await this.device.shell('input swipe 10 1000 10 10');
+        await expect(this.page.getByRole('heading', {name:'3. Wprowadź swój adres i dane kontaktowe'})).toBeVisible();
+        console.log('Checking: 3. Wprowadź swój adres i dane kontaktowe')
+    }
+}
--- tests/visual_regression.spec.ts
+++ tests/visual_regression.spec.ts
@@ -1,8 +1,8 @@
 import { _android as android } from "playwright";
 import {test, expect, _android}   from "@playwright/test";
-import { text } from "stream/consumers";
+import { TwentyFourMxPage } from './24mx_pom.page';
 
-
+const percySnapshot = require('@percy/playwright');
 test('Add product to basket', async () => {
     const [device] = await android.devices();
     console.log(`Model: ${device.model()}`);
@@ -11,106 +11,74 @@ test('Add product to basket', async () => {
     // Before actions
     await device.shell('pm clear com.android.chrome');
     await device.shell('am set-debug-app --persistent com.android.chrome');
-    // Take screenshot of the whole device.
-    await device.screenshot({path:'tests/screenshots/' + 'debug.png'});
 
-    // Launch Chrome browser.
+    //Launch Chrome browser.
     await device.shell('am force-stop com.android.chrome');
     const context = await device.launchBrowser();
 
-    // Use BrowserContext as usual.
     const page = await context.newPage();
-    await page.goto('https://www.24mx.pl/');
-    console.log(await page.evaluate(() => window.location.href));
-    if (await page.getByRole('button',{name:"OK"}).count()>0)
-        await page.getByRole('button',{name:"OK"}).click()
-    await page.getByRole('img', {name:'Kaski MX'}).click()
-    page.waitForLoadState('domcontentloaded')
-    page.click("(//img[@title='Kask Cross Raven Airborne Evo Czarny'])[1]");
-    await device.shell('input swipe 10 800 10 10');
-    await page.screenshot({path:'tests/screenshots/' + 'debug.png'});
-    await page.click("//div[@class='m-select__display']");
-    await page.click("(//div[@class='a-product-variation'])[3]");
-    await page.click(".m-button.m-button--purchase.qa-pdp-add-to-cart-btn.m-button--md");
-    page.waitForLoadState('domcontentloaded')
-    await page.click("//a[contains(text(),'Przejdź do kasy')]");
-    await page.waitForLoadState('domcontentloaded');
-    await page.waitForSelector("//div[@class='m-checkout-box m-checkout-box--delivery']//div[@class='m-checkout-box__heading']");
-    await Promise.all([
-        expect(page).toHaveScreenshot('/screenshots/koszyk.png', {clip: {x:0, y:0, height:224, width:514}})
-    ])
-    // Go to basket
-    page.waitForLoadState()
-    await page.click("//div[@class='m-header-button m-header-button--icon qa-mobile-header-cart']//fa-icon[@class='ng-fa-icon']//*[name()='svg']");
-    // Proceed to checkout
-    console.log('Proceed to checkout')
-    page.waitForLoadState()
-    await page.click("(//span[contains(text(),'Przejdź do kasy')])[1]");
-    console.log("Clicked Button: Przejdz do kasy")
-    // check address and contact details
-    console.log('Check address and contact details')
-    page.waitForLoadState()
-    await device.shell('input swipe 10 1000 10 10');
-    page.waitForLoadState()
-    await expect(page.getByRole('heading', {name:'1. Wybierz opcję dostawy'})).toBeVisible();
-    console.log('Checking: 1. Wybierz opcję dostawy')
-    await expect(page.getByRole('heading', {name:'2. Wybierz metodę płatności'})).toBeVisible();
-    console.log('Checking: 2. Wybierz metodę płatności')
-    await device.shell('input swipe 10 1000 10 10');
-    await expect(page.getByRole('heading', {name:'3. Wprowadź swój adres i dane kontaktowe'})).toBeVisible();
-    console.log('Checking: 3. Wprowadź swój adres i dane kontaktowe')
+    const twentyFourMxPage = new TwentyFourMxPage(page, device);
+    await twentyFourMxPage.goto()
+    await twentyFourMxPage.chooseHelmetbyTitle("Kask Cross Raven Airborne Evo Czarny")
+    await twentyFourMxPage.chooseHelmetSize('M')
+    await twentyFourMxPage.addToCartAndCheckout()
+    await percySnapshot(page, 'android_koszyk.png');
+    await twentyFourMxPage.compareImage('/screenshots/koszyk.png', {x:0, y:0, height:224, width:514})
+    twentyFourMxPage.goToBasket()
+    twentyFourMxPage.goToCashBox()
+    twentyFourMxPage.check_address_and_contact_details()
     
 });
 
-test('Remove product from basket', async () => {
-    const [device] = await android.devices();
-    console.log(`Model: ${device.model()}`);
-    console.log(`Serial: ${device.serial()}`);
+// test('Remove product from basket', async () => {
+//     const [device] = await android.devices();
+//     console.log(`Model: ${device.model()}`);
+//     console.log(`Serial: ${device.serial()}`);
 
-    // Before actions
-    await device.shell('pm clear com.android.chrome');
-    await device.shell('am set-debug-app --persistent com.android.chrome');
-    // Take screenshot of the whole device.
-    await device.screenshot({path:'tests/screenshots/' + 'debug.png'});
+//     // Before actions
+//     await device.shell('pm clear com.android.chrome');
+//     await device.shell('am set-debug-app --persistent com.android.chrome');
+//     // Take screenshot of the whole device.
+//     await device.screenshot({path:'tests/screenshots/' + 'debug.png'});
 
-    // Launch Chrome browser.
-    await device.shell('am force-stop com.android.chrome');
-    const context = await device.launchBrowser();
+//     // Launch Chrome browser.
+//     await device.shell('am force-stop com.android.chrome');
+//     const context = await device.launchBrowser();
 
-    // Use BrowserContext as usual.
-    const page = await context.newPage();
-    await page.goto('https://www.24mx.pl/');
-    console.log(await page.evaluate(() => window.location.href));
-    if (await page.getByRole('button',{name:"OK"}).count()>0)
-        await page.getByRole('button',{name:"OK"}).click()
-    await page.getByRole('img', {name:'Kaski MX'}).click()
-    page.waitForLoadState('domcontentloaded')
-    page.click("(//img[@title='Kask Cross Raven Airborne Evo Czarny'])[1]");
-    await device.shell('input swipe 10 800 10 10');
-    await page.screenshot({path:'tests/screenshots/' + 'debug.png'});
-    await page.click("//div[@class='m-select__display']");
-    await page.click("(//div[@class='a-product-variation'])[3]");
-    await page.click(".m-button.m-button--purchase.qa-pdp-add-to-cart-btn.m-button--md");
-    page.waitForLoadState('domcontentloaded')
-    await page.click("//a[contains(text(),'Przejdź do kasy')]");
-    await page.waitForLoadState('domcontentloaded');
-    await page.waitForSelector("//div[@class='m-checkout-box m-checkout-box--delivery']//div[@class='m-checkout-box__heading']");
-    await Promise.all([
-        expect(page).toHaveScreenshot('/screenshots/koszyk.png', {clip: {x:0, y:0, height:224, width:514}})
-    ])
-    // Go to basket
-    page.waitForLoadState()
-    await page.click("//div[@class='m-header-button m-header-button--icon qa-mobile-header-cart']//fa-icon[@class='ng-fa-icon']//*[name()='svg']");
-    // Proceed to checkout
-    console.log('Proceed to checkout')
-    page.waitForLoadState()
-    await page.click("(//button[@class='m-button m-button__product-edit m-button--xs m-button--xs--square m-button--navigation--outline qa-edit-remove-btn ng-star-inserted'])[1]");
-    console.log("Clicked Button: Edytuj")
-    page.waitForLoadState()
-    await page.click("//span[contains(text(),'Usuń')]");
-    console.log("Clicked Button: Usuń")
-    await page.waitForSelector('//html/body/app-root/p-header/header/div[1]/div[1]/div/p-header-minicart/p-header-minicart-mobile/div/div/div[1]/div/div/div/div[1]')
-    await page.getByText("Twój koszyk jest pusty")
+//     // Use BrowserContext as usual.
+//     const page = await context.newPage();
+//     await page.goto('https://www.24mx.pl/');
+//     console.log(await page.evaluate(() => window.location.href));
+//     if (await page.getByRole('button',{name:"OK"}).count()>0)
+//         await page.getByRole('button',{name:"OK"}).click()
+//     await page.getByRole('img', {name:'Kaski MX'}).click()
+//     page.waitForLoadState('domcontentloaded')
+//     page.click("(//img[@title='Kask Cross Raven Airborne Evo Czarny'])[1]");
+//     await device.shell('input swipe 10 800 10 10');
+//     await page.screenshot({path:'tests/screenshots/' + 'debug.png'});
+//     await page.click("//div[@class='m-select__display']");
+//     await page.click("(//div[@class='a-product-variation'])[3]");
+//     await page.click(".m-button.m-button--purchase.qa-pdp-add-to-cart-btn.m-button--md");
+//     page.waitForLoadState('domcontentloaded')
+//     await page.click("//a[contains(text(),'Przejdź do kasy')]");
+//     await page.waitForLoadState('domcontentloaded');
+//     await page.waitForSelector("//div[@class='m-checkout-box m-checkout-box--delivery']//div[@class='m-checkout-box__heading']");
+//     await Promise.all([
+//         expect(page).toHaveScreenshot('/screenshots/koszyk.png', {clip: {x:0, y:0, height:224, width:514}})
+//     ])
+//     // Go to basket
+//     page.waitForLoadState()
+//     await page.click("//div[@class='m-header-button m-header-button--icon qa-mobile-header-cart']//fa-icon[@class='ng-fa-icon']//*[name()='svg']");
+//     // Proceed to checkout
+//     console.log('Proceed to checkout')
+//     page.waitForLoadState()
+//     await page.click("(//button[@class='m-button m-button__product-edit m-button--xs m-button--xs--square m-button--navigation--outline qa-edit-remove-btn ng-star-inserted'])[1]");
+//     console.log("Clicked Button: Edytuj")
+//     page.waitForLoadState()
+//     await page.click("//span[contains(text(),'Usuń')]");
+//     console.log("Clicked Button: Usuń")
+//     await page.waitForSelector('//html/body/app-root/p-header/header/div[1]/div[1]/div/p-header-minicart/p-header-minicart-mobile/div/div/div[1]/div/div/div/div[1]')
+//     await page.getByText("Twój koszyk jest pusty")
     
-});
+// });
 
